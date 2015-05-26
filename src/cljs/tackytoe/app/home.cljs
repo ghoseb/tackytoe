563d04f7be893e094aa05904dbba780f44d79651
Nayan Suthar <nayan@helpshift.com>
2015-05-26 20:20:58 +0530

    Markup and styles for home page

--- src/cljs/tackytoe/app/home.cljs
+++ src/cljs/tackytoe/app/home.cljs
@@ -1,5 +1,10 @@
 (ns tackytoe.app.home)
 
 (defn home []
-  [:div [:h2 "Welcome to the ultimate Tic Tac Toe game!"]
-   [:div [:a {:href "#/game"} "go to the game page"]]])
+  [:div.player-select
+   [:h2.player-select__msg "Welcome to"]
+   [:h1.player-select__title "Tic Tac Toe"]
+   [:small.player-select__vs "play against"]
+   [:div.player-select__items-list
+    [:a.player-select__item {:href "#/game" :data-player "rajini"}]
+    [:a.player-select__item {:href "#/game" :data-player "batman"}] ]])
