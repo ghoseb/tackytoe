35cca6952f0588809113b40a5385c37f24abce96
Baishampayan Ghose <b.ghose@gmail.com>
2015-05-26 19:32:38 +0530

    Small change.

--- src/cljs/tackytoe/app/game.cljs
+++ src/cljs/tackytoe/app/game.cljs
@@ -3,9 +3,14 @@
 (def players [{:name "Rajnikanth" :email "r@k.com"}
               {:name "Rakhi" :email "foo@bar.com"}])
 
+(defn player
+  [name email]
+  [:li [:a {:href (str "mailto:" email) :target :_blank}
+        name]])
+
+
 (defn game []
   [:div "This is the tic tac toe game page."
-   [:ul (for [p players]
-          [:li [:a {:href (str "mailto:" (:email p)) :target :_blank}
-                (:name p)]])]
+   [:ul (for [{:keys [name email]} players]
+          [player name email])]
    [:div [:a {:href "#/"} "go to the home page"]]])
