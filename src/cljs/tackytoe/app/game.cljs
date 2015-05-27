cd03e053c3e220d88b33971cac2640ab79773e15
Nayan Suthar <nayan@helpshift.com>
2015-05-27 19:33:48 +0530

    Markup for game and results page
    - Add home and restart buttons
    - Animation for solved cells
    - Animation to indicate turn
    - Results page markup

--- src/cljs/tackytoe/app/game.cljs
+++ src/cljs/tackytoe/app/game.cljs
@@ -7,5 +7,18 @@
   [:div.game
    [:div.game__board (for [[idx cell] (map-indexed vector (:board @state))]
                        ^{:key idx} [:div.game__cell {:data-marked cell}])]
-   [:div.game__player {:data-player "batman"}]
-   [:div.game__player {:data-player "you"}]])
+   [:div.game__player {:data-player "batman" :data-turn false}]
+   [:div.game__player {:data-player "user"}]
+   [:a.game__home {:href "/"}]
+   [:a.game__reset {:href "javascript:void(0)"}]])
+
+
+
+
+;; Use this markup to render results -->
+
+;; [:div.result
+;;    [:h2.result__label "Rajini Won"]
+;;    [:div.result__winner {:data-player "rajini"}]
+;;    [:h1.result__text "Mind It!!"]
+;;    [:a.result__home {:href "/"}]]
