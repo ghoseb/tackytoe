2e69175c79e933206242687a2cee091e609d228c
Baishampayan Ghose <b.ghose@gmail.com>
2015-05-29 18:39:23 +0530

    Indentation fix

--- src/cljs/tackytoe/app/home.cljs
+++ src/cljs/tackytoe/app/home.cljs
@@ -8,4 +8,5 @@
    [:small.player-select__vs "play against"]
    [:div.player-select__items-list
     (for [p players]
-      ^{:key (:name p)} [:a.player-select__item {:href (:url p) :data-player (:name p)}])]])
+      ^{:key (:name p)} [:a.player-select__item {:href (:url p)
+                                                 :data-player (:name p)}])]])
